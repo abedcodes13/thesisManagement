483a25e6d428a8e4e59179f6dd7d53bb035e7622
abedscodes <s361222@students.cdu.edu.au>
2026-03-14 06:00:45 +0930

    fix host

--- proj/settings.py
+++ proj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3j1tg2&8b^dz4jopdp7fwhn@jw+f_lfkiqxf16m%4t2)*qwwbz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thesismanagement.onrender.com/']
+ALLOWED_HOSTS = ['thesismanagement.onrender.com']
 
 
 # Application definition
